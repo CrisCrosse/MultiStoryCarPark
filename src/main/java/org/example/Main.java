29831d717a0f6aeabd0f0fb99a59add88addf81f
chrisrossell <chrisrossell1999@gmail.com>
2025-11-11 20:14:23 +0000

    Modify threads to test max capacity

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 100; i++) {
             System.out.println("creating a thread");
 //            TODO look into platform threads and why the runnable isn't being executed'
             var thread = new Thread(new ParkSomeCarsThenMakeThemLeave());
--- src/main/java/org/example/ParkSomeCarsThenMakeThemLeave.java
+++ src/main/java/org/example/ParkSomeCarsThenMakeThemLeave.java
@@ -14,20 +14,20 @@ public class ParkSomeCarsThenMakeThemLeave implements Runnable {
         var truck = new Truck();
         var motorcycle = new Motorcycle();
 
-        carParkInstance.enter(car);
-        carParkInstance.enter(truck);
-        carParkInstance.enter(motorcycle);
+        var carParked = carParkInstance.enter(car);
+        var truckParked = carParkInstance.enter(truck);
+        var motorcycleParked = carParkInstance.enter(motorcycle);
 
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException ignored) {
-            return;
-        }
-        System.out.printf("In Thread %s and about to make three vehicles leave%n", Thread.currentThread().threadId());
-
-
-        carParkInstance.leave(car);
-        carParkInstance.leave(truck);
-        carParkInstance.leave(motorcycle);
+        System.out.printf("In Thread %s, car parked: %b truck parked: %b motorcycle parked: %b%n",
+                Thread.currentThread().threadId(), carParked, truckParked, motorcycleParked);
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException ignored) {
+//            return;
+//        }
+//        System.out.printf("In Thread %s and about to make three vehicles leave%n", Thread.currentThread().threadId());
+//        carParkInstance.leave(car);
+//        carParkInstance.leave(truck);
+//        carParkInstance.leave(motorcycle);
     }
 }
